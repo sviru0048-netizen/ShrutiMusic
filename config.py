2b9ffa4c441cc32994374119a5ee3fa5b6f95633
KRISH X STAR <sviru0048@gmail.com>
2026-05-19 23:44:59 +0530

    Update support channel URL in config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/NoxxOP/ShrutiMusi
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/ShrutiBots")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/KRISH_HACKER_OP")
 SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/ShrutiBotsSupport")
 INSTAGRAM = os.getenv("INSTAGRAM", "https://instagram.com/yaduwanshi_nand")
 YOUTUBE = os.getenv("YOUTUBE", "https://youtube.com/@NandEditz")
